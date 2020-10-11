898fdbe57ed3c3e9fac3ba4f42065d9a016fea1c
cesarvin <vinizio007@gmail.com>
2020-10-10 20:55:42 -0600

    llamada al servicio

--- frontend/proyecto2/src/App.js
+++ frontend/proyecto2/src/App.js
@@ -2,7 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function callApiGET(){
+  fetch("http://127.0.0.1:8000/testget/", {
+    "method": "GET",
+    "Access-Control-Allow-Origin": "http://127.0.0.1:8000/"
+  })
+  .then(response => response.json())
+  .then(response => {
+    console.log('llamada al api ------------>',response.response)
+  })
+  .catch(err => { console.log(err); 
+  });
+
+ 
+}
+
+
 function App() {
+  // llamada al servicio
+  callApiGET()
+  //fin
+  
   return (
     <div className="App">
       <header className="App-header">
