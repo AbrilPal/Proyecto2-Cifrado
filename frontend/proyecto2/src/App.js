58327771b443bc0373d3c7cea594b2a3d334734e
Crismaria11 <bau161260@uvg.edu.gt>
2020-10-11 14:44:50 -0600

    Login

--- frontend/proyecto2/src/App.js
+++ frontend/proyecto2/src/App.js
@@ -5,12 +5,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           QUE ONDA PERROS! ♥
         </p>
-      </header>
+      </header> */}
+      <section className="loginDiv">
+        <div className="loginForm">
+          <h1 class="title">Login de usuarios</h1>
+          <br />
+          <input className="controls" type="text" placeholder="Username"/>
+          <br />
+          <input className="controls" type="password" placeholder="Password"/>
+          <br />
+          <input className="button" type="submit" placeholder="Login"/>
+        </div>
+      </section>
     </div>
   );
 }
